28ee98920bc2f8ad95928f65d0c14dbebe13be6d
Jay Blunt <jayblunt@jf.33p.lan>
2020-01-08 12:49:24 -0800

    Initial Checkin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ESI_Flask_Authlib_SSO
-ESI Single Sign On sample with Flask and Authlib
+# ESI Single Sign On sample with Flask and Authlib
+
+The [Eve Online](https://www.eveonline.com/) [Swagger Interface](https://esi.evetech.net/ui/) uses OAUTH2 for authorization. 
+
+There is a CCP blog entry ([ESI STEP BY STEP - SSO TO AUTHENTICATED CALLS](https://developers.eveonline.com/blog/article/sso-to-authenticated-calls)) that walks through the process.
+
+There is also a repository example by @Kyria [here](https://github.com/Kyria/flask-esipy-example).
+
+[Authlib](https://authlib.org/) makes the process much simpler. 
+
+This snippet uses Authlib and Flask to implement basic SSO for Eve. It assumes that you have a registered [application](https://developers.eveonline.com/applications) that you are using.
+
+As of writing, the Eve SSO is not OpenID compatible - the OpenID scopes are not available and there is no standard id_token. This example works around this by decoding the access token and using that as the userinfo.
---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, url_for, session
+from flask import render_template, redirect
+from authlib.integrations.flask_client import OAuth
+from authlib.jose import jwt
+
+import app_config
+
+
+app = Flask(__name__)
+app.config.from_object('app_config')
+
+
+
+
+oauth = OAuth(app)
+oauth.register(
+    name = 'eveonline',
+    server_metadata_url = 'https://login.eveonline.com/.well-known/oauth-authorization-server',
+    client_kwargs = {
+        'scope': 'publicData',
+        'datasource': 'tranquility'
+    }
+)
+
+
+
+
+@app.route('/')
+def homepage():
+    user = session.get('user')
+    return render_template('home.html', user=user)
+
+
+
+
+@app.route('/sso/login')
+def sso_login():
+    redirect_uri = url_for('sso_callback', _external=True, _scheme='https')
+    return oauth.eveonline.authorize_redirect(redirect_uri)
+
+
+
+
+@app.route('/sso/callback')
+def sso_callback():
+    token = oauth.eveonline.authorize_access_token()
+
+    ## Find the signature key
+    t_jwk_set = oauth.eveonline._fetch_jwk_set()
+    jwt_signature_key = None
+    for x in list(t_jwk_set.get('keys') or []):
+        if x.get('kid') == 'JWT-Signature-Key':
+            jwt_signature_key = x
+            break
+
+    ## ESI does not (currently) offer an openid id_token.
+    ## Here we decode the ESI access_token that we are offered
+    if jwt_signature_key:
+        rval = jwt.decode(token['access_token'], jwt_signature_key)
+        if rval:
+            session['user'] = rval
+
+    return redirect('/')
+
+
+
+
+@app.route('/sso/logout')
+def sso_logout():
+    session.pop('user', None)
+    return redirect('/')
+
+
+
+
+if __name__ == '__main__':
+    if app_config.DEBUG:
+        app.run(port=app_config.PORT, host=app_config.HOST)
+    else:
+        import waitress
+        waitress.serve(app, port=app_config.PORT, host=app_config.HOST)
+
+
+
